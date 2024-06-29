1c3a21ca25b77d977283a3b9bd7adcda63093df4
nb923 <nb923@scarletmail.rutgers.edu>
2024-06-29 19:18:25 -0400

    Add basic game functionality

--- frontend/app.js
+++ frontend/app.js
@@ -1,21 +1,32 @@
 const termList = [];
+const answer = [];
+answerButton = -1;
 
 function addPair(event) 
 {
     event.preventDefault();
     const keyInput = document.getElementById('term');
     const valueInput = document.getElementById('definition');
+    const logger = document.getElementById('logger');
 
     const term = keyInput.value;
     const definition = valueInput.value;
 
-    if(term && definition && !termList.some(pair => pair[0] == term && pair[1] == definition)) {
-        termList.push([term, definition]);
+    if(term && definition) {
+        if (!termList.some(pair => pair[0] == term && pair[1] == definition))
+        {
+            termList.push([term, definition]);
 
-        keyInput.value = '';
-        valueInput.value = '';
-
-        updateList();
+            keyInput.value = '';
+            valueInput.value = '';
+    
+            updateList();
+            logger.innerHTML = '';
+        }
+        else
+        {
+            logger.innerHTML = 'Error: Element already exists in list';
+        }
     }
 }
 
@@ -30,4 +41,136 @@ function updateList()
     })
 }
 
+function startGame(event)
+{
+    event.preventDefault();
+
+    changeGameValues();
+}
+
+function changeGameValues()
+{
+    const logger = document.getElementById('logger');
+
+    if (termList.length > 1)
+    {
+        answerNum = Math.floor(Math.random() * termList.length);
+        otherNum = Math.floor(Math.random() * termList.length);
+        const buttonOne = document.getElementById('buttonOne');
+        const buttonTwo = document.getElementById('buttonTwo');
+        const definitionText = document.getElementById('elementDefinition');
+
+        while (otherNum == answerNum)
+        {
+            otherNum = Math.floor(Math.random() * termList.length);
+        }
+
+        chooseButton = Math.floor(Math.random() * 2);
+        definitionText.innerHTML = termList[answerNum][1];
+
+        if (chooseButton == 0)
+        {
+            buttonOne.innerHTML = termList[answerNum][0];
+            buttonTwo.innerHTML = termList[otherNum][0];
+            answerButton = 1;
+        }
+        else
+        {
+            buttonTwo.innerHTML = termList[answerNum][0];
+            buttonOne.innerHTML = termList[otherNum][0];
+            answerButton = 2;
+        }
+
+        logger.innerHTML = '';
+    }
+    else
+    {
+        logger.innerHTML = 'Error: Not enough pairs for game to start. Have 2+ pairs for game to start';
+    }
+}
+
+function gameMoveButtonOne(event)
+{
+    event.preventDefault();
+    console.log(answerButton);
+
+    const logger = document.getElementById('logger');
+
+    if (termList.length > 1)
+    {
+        if (answerButton > 0)
+        {
+            if (answerButton == 1)
+            {
+                changeGameValues();
+                logger.innerHTML = '';
+            }
+            else
+            {
+                answerButton = -1;
+                logger.innerHTML = 'Wrong answer, game is stopped';
+
+                const buttonOne = document.getElementById('buttonOne');
+                const buttonTwo = document.getElementById('buttonTwo');
+                const definitionText = document.getElementById('elementDefinition');
+
+                buttonOne.innerHTML = 'Term1';
+                buttonTwo.innerHTML = 'Term2';
+                definitionText.innerHTML = 'Definition';
+            }
+        }
+        else
+        {
+            logger.innerHTML = 'Error: Game has not been started yet';
+        }
+    }
+    else
+    {
+        logger.innerHTML = 'Error: Not enough pairs for game to start. Have 2+ pairs for game to start';
+    }
+}
+
+function gameMoveButtonTwo(event)
+{
+    event.preventDefault();
+
+    const logger = document.getElementById('logger');
+
+    if (termList.length > 1)
+    {
+        if (answerButton > 0)
+        {
+            if (answerButton == 2)
+            {
+                changeGameValues();
+                logger.innerHTML = '';
+            }
+            else
+            {
+                answerButton = -1;
+                logger.innerHTML = 'Wrong answer, game is stopped';
+
+                const buttonOne = document.getElementById('buttonOne');
+                const buttonTwo = document.getElementById('buttonTwo');
+                const definitionText = document.getElementById('elementDefinition');
+
+                buttonOne.innerHTML = 'Term1';
+                buttonTwo.innerHTML = 'Term2';
+                definitionText.innerHTML = 'Definition';
+            }
+        }
+        else
+        {
+            logger.innerHTML = 'Error: Game has not been started yet';
+        }
+    }
+    else
+    {
+        logger.innerHTML = 'Error: Not enough pairs for game to start. Have 2+ pairs for game to start';
+    }
+}
+
 document.getElementById('addDefinition').addEventListener('click', addPair);
+document.getElementById('startGameButton').addEventListener('click', startGame);
+document.getElementById('buttonOne').addEventListener('click', gameMoveButtonOne);
+document.getElementById('buttonTwo').addEventListener('click', gameMoveButtonTwo);
